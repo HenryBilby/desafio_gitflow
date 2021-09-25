6847cfc4293af5604fcd5e7b7a0147fc58c9c00e
Henry Miller Maia Bilby <hmmb-eng@outlook.com>
2021-09-25 18:06:45 -0400

    Adicionando metodo que retorna pessoa aleatoria

--- desafio_gitflow/desafio_gitflow/ViewController.swift
+++ desafio_gitflow/desafio_gitflow/ViewController.swift
@@ -9,11 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let pessoasEngenheiras : [Pessoa] = [Pessoa(nome: "Joao Engenharia"),
+                                         Pessoa(nome: "Maria Engenharia"),
+                                         Pessoa(nome: "José Engenharia"),
+                                         Pessoa(nome: "Marta Engenharia")]
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    private func getPessoaAleatoria(pessoas:[Pessoa]) -> Pessoa {
+        guard let pessoaAleatoria = pessoas.randomElement() else {
+            let indice = Int(arc4random_uniform(UInt32(pessoas.count)))
+            return pessoas[indice]
+        }
+
+        return pessoaAleatoria
+    }
 
 }
 
