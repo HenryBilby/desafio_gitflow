e2245701e7767b2bacd635eedd8425f3ea9117f1
Paula Higa <paulahiga@msn.com>
2021-09-24 21:59:12 -0300

    adicionando tela inicial

---
+++ desafio_gitflow/desafio_gitflow/Profissao.swift
@@ -0,0 +1,22 @@
+//
+//  Profissao.swift
+//  desafio_gitflow
+//
+//  Created by Paula Matsumoto on 24/09/21.
+//
+
+import Foundation
+
+class Profissao {
+    
+    let descricao: String
+    
+    init(descricao:String) {
+        
+        self.descricao = descricao
+}
+    func calculaSalario(valorHora: Double) -> Double {
+        
+        return valorHora * 220
+    }
+}
